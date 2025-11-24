a789b98af83c73718c58e28b5179bd19a99c5769
Staubgeborener <eric.schroeder@protonmail.com>
2025-11-24 11:48:59 +0100

    Add —db as standalone flag in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ if [ "$EPISODES_UPDATE" = "true" ]; then
     python /app/acepace.py --episodes_update
 fi
 
+if [ "$DB" = "true" ]; then
+    python /app/acepace.py --db
+fi
+
 exec python /app/acepace.py \
     ${NYAA_URL:+--url "$NYAA_URL"} \
     ${TORRENT_CLIENT:+--download "$TORRENT_CLIENT"} \
-    ${DB:+--db}
